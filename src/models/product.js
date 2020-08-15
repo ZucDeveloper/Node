1c69698a582ca437d06c5c1c02362c3632f594bd
Antonio <zucdeveloper@gmail.com>
2020-08-15 05:22:44 -0300

    Modelo de esquema do produto

---
+++ src/models/product.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const mongoose = require('mongose');
+const Schema = mongoose.Schema;
+
+const Schema = new Schema({
+  // Schema cria um Id de cada produto dinamicamente
+  // Titulo tipo string, 
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  slug: {
+    type:String,
+    required: true,
+    trim: true,
+    index: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true
+   
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  active: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+  tags: [{
+    type: String,
+    required: true
+  }]
+});
+
+// Modelo de saida deste documento
+// {
+//   "title": "titulo",
+//   "description": "xpto",
+//   slug...
+
+//   "tags": [
+//     "teste", "123", "Pessoas"
+//   ]
+// }
+
+
+module.exports = mongoose.model('Product', schema);
+
