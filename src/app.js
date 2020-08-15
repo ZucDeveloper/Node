0f0a28cf0138dcaa0dcd77aa1e66cb1658a7c870
Antonio <zucdeveloper@gmail.com>
2020-08-15 05:04:13 -0300

    Conexão com Banco de dados Mongoose

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
 
 const app = express();
 const router = express.Router();
 
+// Conecta com o banco de dados
+
+mongoose.connect ("mongodb+srv://flamengo110493:flamengo110493@meudb.rifzw.mongodb.net/MeuDb?retryWrites=true&w=majority", {useNewUrlParser: true} )
 // Carrega as Rotas
 const indexRoute = require('./routes/index-route');
 const productsRoute = require('./routes/product-route');
