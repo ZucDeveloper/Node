2b5a4f341fe6f3383701ee5e9845cb3c6af7a7c6
Antonio <zucdeveloper@gmail.com>
2020-08-15 01:15:02 -0300

    Separando Servidor e App

--- bin/server.js
+++ bin/server.js
@@ -1,25 +1,10 @@
 'use strict'
-
+const app = require('../src/app');
 const http = require('http');
 const debug = require('debug')('nodestr:server');
-const express = require('express');
-
-
-const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
-
-
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-  res.status(200).send({
-    title: "Node Store API",
-    version: "0.0.1"
-  });
-});
-app.use('/', route);
 
 server.listen(port);
 server.on('err', onError);
@@ -27,6 +12,7 @@ server.log('API rodando na porta ' + port);
 
 console.log('API rodando na porta ' + port);
 
+// Functions
 function onListening() {
   const addr =server.address();
   const bind = typeof addr === 'string'
@@ -37,7 +23,6 @@ function onListening() {
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
-
   if (isNaN(port)) {
     return val;
   }
