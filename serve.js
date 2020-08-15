cd05357c4e5f028e9ecdd62f7fbd2624cd2e9598
Antonio <zucdeveloper@gmail.com>
2020-08-15 00:36:19 -0300

    Normalizando porta

--- serve.js
+++ serve.js
@@ -6,7 +6,7 @@ const express =require('express');
 
 
 const app =express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 
@@ -22,4 +22,16 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
-console.log('API rodando na porta' + port);
+console.log('API rodando na porta ' + port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
