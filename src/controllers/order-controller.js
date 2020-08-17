ea0077cb0d0f926ba9080155a89116b635d5d601
Antonio <zucdeveloper@gmail.com>
2020-08-17 08:35:14 -0300

    Recuperando dados do usuário Logado

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -3,6 +3,7 @@
 // const ValidationContract = require('../validators/fluentValidator');
 const repository = require('../repositories/ordersRepository')
 const guid = require('guid');
+const authService = require('../services/authService');
 
 // Busca os item do banco de dados
 exports.get = async (req, res, next) => {
@@ -18,13 +19,18 @@ exports.get = async (req, res, next) => {
 
 exports.post = async (req, res, next) => {
   try {
+    // Recupera o Token
+    const token = req.body.token || req.query.token || req.headers['x-access-'];
+    const data = await authService.decodeToken(token);
+
     await repository.create({
-      customer: req.body.customer,
+      customer: data.id,
       number: guid.raw().substring(0, 6),
       items: req.body.items
     });
     res.status(201).send({ message: 'Pedido cadastrado com sucesso!' });
   } catch (e) {
+    console.log(e)
     res.status(400).send({
       message: 'Falha ao processar sua requisição'
     });
